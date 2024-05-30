7afc098aaccd32c9188c3f8ec08c71aefacd8314
aryawarti <yashishkumar996@gmail.com>
2024-05-30 14:24:23 +0530

    About Us page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: (theme) => ({
+        "gradient-to-r": "linear-gradient(to right,  #141416,#231F20 )",
+      }),
       colors: {
-        'neutralsilver':'#F5F7FA',
-        'neutralDGrey':'#4D4D4D',
-        'brandPrimary':'#4CAF4F',
-        'neutralDGrey':'#717171',
-      }
+        neutralsilver: "#F5F7FA",
+        neutralDGrey: "#4D4D4D",
+        brandPrimary: "#4CAF4F",
+        neutralDGrey: "#717171",
+        wholeBackground: "#231F20",
+      },
     },
   },
   plugins: [],
-}
+};
 
