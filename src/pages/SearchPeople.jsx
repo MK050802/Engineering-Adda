572a0ef195b71d43f10e30a92ca82c1e234f2eef
aryawarti <yashishkumar996@gmail.com>
2024-07-05 21:06:58 +0530

    Share notes

--- src/pages/SearchPeople.jsx
+++ src/pages/SearchPeople.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import arya from "./images/arya.jpg"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import arya from "./images/arya.jpg";
 
 const SearchComponent = () => {
   const [query, setQuery] = useState("");
@@ -8,22 +10,22 @@ const SearchComponent = () => {
       id: 1,
       name: "Sweety Devi",
       college: "Mad House University",
-      branch: "Power BI ",
-      place: "Khachi",
+      branch: "Power BI || Data Structre and algo || Web Developer  || ECE",
+      place: "Khachi,Bihar 2.0",
     },
     {
       id: 2,
       name: "Guddu Pandit",
       college: "Tribal Government School",
-      branch: "ECE",
-      place: "Vashepur",
+      branch: "flutter developer || Data Analyst || Web Developer || CS",
+      place: "Vashepur, Jharkhad",
     },
     {
       id: 3,
       name: "Vinod Khanna",
-      college: "NIT Kurukshetra",
-      branch: "CS",
-      place: "Mumbai",
+      college: "NIT Kurukshetra, Haryana",
+      branch: "Artificial Intelligence || Matchine Learning || DSA || PI",
+      place: "Mumbai, Maharastra",
     },
     {
       id: 4,
@@ -33,35 +35,35 @@ const SearchComponent = () => {
       place: "Kashi",
     },
     {
-      id: 4,
+      id: 5,
       name: "Bauwa ",
       college: "Purv madhyamik vidhyalay , Chaklala",
       branch: "Mathematics",
       place: "Varanasi,Uttar Pradesh",
     },
     {
-      id: 4,
+      id: 6,
       name: "Ashish Kumar Yadav",
       college: "Allahabad University",
       branch: "Phylosophy",
       place: "Kashi",
     },
     {
-      id: 4,
+      id: 7,
       name: "Aryawarti",
       college: "Banaras Hindu University",
       branch: "Astro Physics",
       place: "Banaras",
     },
     {
-      id: 4,
+      id: 8,
       name: "Shantanu Singh",
       college: "Patna University",
       branch: "Sprituality",
       place: "Rohtash ,Bihar",
     },
     {
-      id: 4,
+      id: 9,
       name: "Aryawarti",
       college: "Banaras Hindu University",
       branch: "Phylosophy",
@@ -82,30 +84,38 @@ const SearchComponent = () => {
 
   return (
     <div className="min-h-screen flex items-center justify-center mt-24 bg-sky-600">
-      <div className="bg-white p-8  rounded shadow-md w-full max-w-3xl">
+      <div className="bg-white p-8 rounded shadow-md w-full max-w-3xl">
         <h1 className="text-2xl font-bold mb-4">Search People</h1>
-        <input
-          type="text"
-          className="w-full p-2 border border-gray-300 rounded mb-4"
-          placeholder="Search by name, college, branch, or place..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
+        <div className="relative mb-4">
+          <input
+            type="text"
+            className="w-full p-3 border border-gray-300 rounded-lg pl-10 focus:outline-none focus:border-blue-500 transition duration-200"
+            placeholder="Search by name, college, branch, or place..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <FontAwesomeIcon
+            icon={faSearch}
+            className="absolute left-4 top-4 right-2 text-gray-400"
+          />
+        </div>
         <ul>
           {filteredPeople.map((person) => (
-            <li key={person.id} className="p-2 border-b border-gray-200">
-              <div className="flex items-center">
+            <li key={person.id} className="flex p-2 border-b border-gray-200">
+              <div>
                 <img
-                  className="w-14 h-14 rounded-full object-cover"
+                  className="w-14 h-14 rounded-full object-cover cursor-pointer"
                   src={arya}
-                  alt={arya}
+                  alt="Profile"
                 />
-                <div className="font-bold ml-5">{person.name}</div>
               </div>
-              <div className="ml-16 p-3 pt-0">
+              <div className="flex flex-col ml-6 gap-1">
+                <div className="font-bold cursor-pointer">{person.name}</div>
+                <div className="text-sm text-gray-600 font-semibold">
+                  {person.branch}
+                </div>
                 <div className="text-sm text-gray-600">{person.college}</div>
-                <div className="text-sm text-gray-600">{person.branch}</div>
-                <div className="text-sm text-gray-600">{person.place}</div>
+                <div className="text-sm text-gray-500">{person.place}</div>
               </div>
             </li>
           ))}
